0404d5c4b0e29315fc0c6d9321de59c0f017efe0
Dennis Mwea <mweadennis2@gmail.com>
2020-05-20 12:19:41 +0300

    Trial code

---
+++ android/app/src/main/java/com/example/native_splash/CustomSplashScreen.java
@@ -0,0 +1,61 @@
+//package name will differ based on projects
+package com.example.native_splash;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import io.flutter.embedding.android.SplashScreen;
+import android.os.Handler;
+
+public class CustomSplashScreen implements SplashScreen {
+  private CustomSplashScreenView splashView;
+
+  @Nullable
+  public View createSplashView(@NonNull Context context, @Nullable Bundle bundle) {
+    if (splashView == null) {
+      splashView = new CustomSplashScreenView(context);
+      splashView.restoreSplashState(bundle);
+    }
+
+    return splashView;
+  }
+
+  //closing the splash
+  public void transitionToFlutter(Runnable onTransitionComplete) {
+//    if (splashView != null) {
+//      splashView.animateAway(onTransitionComplete);
+//    }
+//    else {
+//      onTransitionComplete.run();
+//     }
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        if (splashView != null) {
+          splashView.animateAway(onTransitionComplete);
+        }
+        else {
+          onTransitionComplete.run();
+         }
+      }
+    }, 1500);
+  }
+
+  //state saving
+  @Override
+  public boolean doesSplashViewRememberItsTransition() {
+    return true;
+  }
+
+  @Override
+  @Nullable
+  public Bundle saveSplashScreenState() {
+    if (splashView != null) {
+     return splashView.saveSplashState();
+    } else {
+      return null;
+    }
+  }
+}
